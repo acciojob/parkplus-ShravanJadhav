85a6c2a1a22b39cb1caadbe712cda9d722299fc1
ShravanJadhav <shravanjadhav844@gmail.com>
2023-03-25 13:36:24 +0530

    parkplus_sol1

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -1,5 +1,7 @@
 package com.driver.services.impl;
 
+import com.driver.model.ParkingLot;
+import com.driver.model.Spot;
 import com.driver.model.SpotType;
 import com.driver.repository.ParkingLotRepository;
 import com.driver.repository.SpotRepository;
@@ -18,26 +20,62 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     SpotRepository spotRepository1;
     @Override
     public ParkingLot addParkingLot(String name, String address) {
+        ParkingLot parkingLot = new ParkingLot(name, address);
+        parkingLotRepository1.save(parkingLot);
 
+        return parkingLot;
     }
 
     @Override
     public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour) {
 
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        Spot spot = new Spot(pricePerHour);
+
+        if(numberOfWheels==2)
+            spot.setSpotType(SpotType.TWO_WHEELER);
+        else if(numberOfWheels==4)
+            spot.setSpotType(SpotType.FOUR_WHEELER);
+        else
+            spot.setSpotType(SpotType.OTHERS);
+
+        spot.setOccupied(true);
+        spot.setParkingLot(parkingLot);
+
+        parkingLotRepository1.save(parkingLot);
+        spotRepository1.save(spot);
+
+        return spot;
+
     }
 
     @Override
     public void deleteSpot(int spotId) {
-
+        parkingLotRepository1.deleteById(spotId);
     }
 
     @Override
     public Spot updateSpot(int parkingLotId, int spotId, int pricePerHour) {
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        List<Spot>spotList = parkingLot.getSpotList();
+        Spot spot = null;
+        for(Spot spot1:spotList){
+            if(spot1.getId()==spotId){
+                spot1.setPricePerHour(pricePerHour);
+                spot=spot1;
+            }
+        }
+        parkingLot.setSpotList(spotList);
+
+        parkingLotRepository1.save(parkingLot);
+        spotRepository1.save(spot);
+
+        return spot;
 
     }
 
     @Override
     public void deleteParkingLot(int parkingLotId) {
-
+        parkingLotRepository1.deleteById(parkingLotId);
     }
 }
--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.driver.services.impl;
 
+import com.driver.model.Payment;
 import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
@@ -17,5 +19,33 @@ public class PaymentServiceImpl implements PaymentService {
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
 
+        Reservation reservation = reservationRepository2.findById(reservationId).get();
+
+        int billPrice = reservation.getNumberOfHours()*reservation.getSpot().getPricePerHour();
+
+        if(amountSent<billPrice){
+            throw new Exception("Insufficient Amount");
+        }else{
+
+            if(mode.equalsIgnoreCase("cash") || mode.equalsIgnoreCase("card") || mode.equalsIgnoreCase("upi")){
+                Payment payment = new Payment();
+                if(mode.equalsIgnoreCase("cash"))
+                    payment.setPaymentMode(PaymentMode.CASH);
+                else if (mode.equalsIgnoreCase("card")) {
+                    payment.setPaymentMode(PaymentMode.CARD);
+                }else
+                    payment.setPaymentMode(PaymentMode.UPI);
+
+                payment.setPaymentCompleted(true);
+                payment.setReservation(reservation);
+
+                reservationRepository2.save(reservation);
+                paymentRepository2.save(payment);
+                return payment;
+            }
+            else{
+                throw new Exception("Payment mode not detected");
+            }
+        }
     }
 }
